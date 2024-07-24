e8d63e45c307c1c44e8a973c4fbec515fbcdc549
pedroluccsousa <pedroluccsousa@icloud.com>
2024-07-24 14:45:25 -0300

    Commit Final

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-console.log("Pedro lucas");
+//Desafio de código n°1
 
+//Variáveis
+let  hero1 = 8005;
+let heroLevels = [
+    {level: 9000, message: "Seu herói é Mestre"},
+    {level: 8000, message:"Seu herói é Diamante"},
+    {level: 7000, message:"Seu herói é Platina"},
+    {level: 6000, message:"Seu herói é Ouro"},
+    {level: 5000, message: "Seu herói é Prata"}
+];
+let heroMessage = "Seu herói é grão-mestre"; //Padrão
+
+//Condicionais
+for (let i = 0; i < heroLevels.length; i++) {
+    if (hero1 >= heroLevels[i].level){
+        heroMessage= heroLevels[i].message;
+        break;
+    }
+}
+
+console.log (heroMessage)
